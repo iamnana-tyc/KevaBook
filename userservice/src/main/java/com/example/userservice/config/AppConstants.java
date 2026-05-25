c4104bee8b27be2f46e0217ac6457e3afd6211ac
iamnana-tyc <nanatyc7@gmail.com>
2026-05-25 11:15:24 +0300

    App constant for pagination

---
+++ userservice/src/main/java/com/example/userservice/config/AppConstants.java
@@ -0,0 +1,9 @@
+package com.example.userservice.config;
+
+public class AppConstants {
+    public static final String PAGE_NUMBER = "0";
+    public static final String PAGE_SIZE = "10";
+    public static final String SORT_USERS_BY= "userId";
+    public static final String SORT_DIRECTION = "asc";
+}
+
