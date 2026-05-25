a23da7c046dd3f2275fe5a5c27aa7a199f674b10
iamnana-tyc <nanatyc7@gmail.com>
2026-05-25 11:19:29 +0300

    Add eureka service

---
+++ eurekaservice/src/test/java/com/iamnana/eurekaservice/EurekaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamnana.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
