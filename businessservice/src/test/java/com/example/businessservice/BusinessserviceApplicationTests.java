126eb62702d0534d991936a4cc8397496c65b231
iamnana-tyc <nanatyc7@gmail.com>
2026-05-25 11:46:55 +0300

    Add business service

---
+++ businessservice/src/test/java/com/example/businessservice/BusinessserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.businessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
