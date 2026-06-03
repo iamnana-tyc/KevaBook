06d2c8118c369b12ae5e321ef6622c7b730bb32a
iamnana-tyc <nanatyc7@gmail.com>
2026-06-03 17:10:33 +0300

    Add service catalog folder

---
+++ servicecatalogservice/src/main/java/com/iamnana/servicecatalogservice/ServicecatalogserviceApplication.java
@@ -0,0 +1,13 @@
+package com.iamnana.servicecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicecatalogserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicecatalogserviceApplication.class, args);
+	}
+
+}
---
+++ servicecatalogservice/src/test/java/com/iamnana/servicecatalogservice/ServicecatalogserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamnana.servicecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicecatalogserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
