cf33b11fc10f85dc09021dd5b2d51f62b6f511da
iamnana-tyc <nanatyc7@gmail.com>
2026-06-16 19:26:56 +0300

    Delete files

--- businessservice/src/main/java/com/example/businessservice/config/AppConstants.java
+++
@@ -1,9 +0,0 @@
-package com.example.businessservice.config;
-
-public class AppConstants {
-    public static final String PAGE_NUMBER = "0";
-    public static final String PAGE_SIZE = "10";
-    public static final String SORT_USERS_BY= "businessId";
-    public static final String SORT_DIRECTION = "asc";
-}
-
--- businessservice/src/main/java/com/example/businessservice/repository/BusinessRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.businessservice.repository;
-
-import com.example.businessservice.model.Business;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BusinessRepository extends JpaRepository<Business, Long> {
-}
