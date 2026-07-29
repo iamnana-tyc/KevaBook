ee507443e278b7818895b83f84fed54f5fd15789
iamnana-tyc <nanatyc7@gmail.com>
2026-07-29 14:40:02 +0300

    User service logic

---
+++ userservice/src/main/java/com/kevabook/userservice/service/UserServiceImpl.java
@@ -0,0 +1,91 @@
+package com.kevabook.userservice.service;
+
+import com.kevabook.userservice.domain.User;
+import com.kevabook.userservice.dto.CreateUserRequest;
+import com.kevabook.userservice.dto.PartialUpdateUserRequest;
+import com.kevabook.userservice.dto.UserResponse;
+import com.kevabook.userservice.exception.ResourceNotFoundException;
+import com.kevabook.userservice.mapper.UserMapper;
+import com.kevabook.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Transactional
+    @Override
+    public UserResponse createUser(CreateUserRequest request) {
+        log.info("Creating user with email={}", request.getEmail());
+        User user = userMapper.mapToUser(request);
+        User savedUser = userRepository.save(user);
+
+        log.info("User created successfully, userId={}, role={}", savedUser.getUserId(), savedUser.getRole());
+        return userMapper.mapToUserResponse(savedUser);
+    }
+
+    @Override
+    public List<UserResponse> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::mapToUserResponse)
+                .toList();
+    }
+
+    @Override
+    public UserResponse getUser(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new ResourceNotFoundException("User", "id", userId));
+
+        return userMapper.mapToUserResponse(user);
+    }
+
+    @Transactional
+    @Override
+    public UserResponse partialUpdateUser(Long userId, PartialUpdateUserRequest request) {
+        log.info("Updating user with id={}", userId);
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new ResourceNotFoundException("User", "id", userId));
+
+        Optional.ofNullable(request.getFirstName())
+                .filter(name -> !name.isBlank())
+                .ifPresent(user::setFirstName);
+
+        Optional.ofNullable(request.getLastName())
+                .filter(name -> !name.isBlank())
+                .ifPresent(user::setLastName);
+
+        Optional.ofNullable(request.getEmail())
+                .filter(email -> !email.isBlank())
+                .ifPresent(user::setEmail);
+
+        Optional.ofNullable(request.getPhoneNumber())
+                .filter(number -> !number.isBlank())
+                .ifPresent(user::setPhoneNumber);
+
+        User saveUser = userRepository.save(user);
+
+        log.info("Successfully updated user id={}", userId);
+        return userMapper.mapToUserResponse(saveUser);
+    }
+
+    @Transactional
+    @Override
+    public void deleteUser(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new ResourceNotFoundException("User", "id", userId));
+
+        userRepository.delete(user);
+    }
+
+
+}
