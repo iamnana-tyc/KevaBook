56eac3380f167d2444987f482c8ce9e85e6d06c8
iamnana-tyc <nanatyc7@gmail.com>
2026-07-29 14:39:01 +0300

    Add user domain class

---
+++ userservice/src/main/java/com/kevabook/userservice/domain/Role.java
@@ -0,0 +1,7 @@
+package com.kevabook.userservice.domain;
+
+public enum Role {
+    ADMIN,
+    BUSINESS_OWNER,
+    BUSINESS_STAFF
+}
---
+++ userservice/src/main/java/com/kevabook/userservice/domain/User.java
@@ -0,0 +1,40 @@
+package com.kevabook.userservice.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Table(name = "users")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
