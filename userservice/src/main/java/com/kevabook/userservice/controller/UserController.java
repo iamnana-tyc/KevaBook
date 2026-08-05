f1e8c40a7943629c8bd3867b2a61d523ef65ebdc
iamnana-tyc <nanatyc7@gmail.com>
2026-08-05 17:23:08 +0300

    Remove unused imports

--- userservice/src/main/java/com/kevabook/userservice/controller/UserController.java
+++ userservice/src/main/java/com/kevabook/userservice/controller/UserController.java
@@ -4,7 +4,6 @@ import com.kevabook.userservice.dto.*;
 import com.kevabook.userservice.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
